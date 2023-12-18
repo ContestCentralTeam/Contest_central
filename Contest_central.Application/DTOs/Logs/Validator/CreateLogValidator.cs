ff693ecd1b411b58f31359c957d2bf6f46fe1506
Abdulsemed-muhaba <abdulsemed.muhaba@a2sv.org>
2023-12-18 18:02:50 +0300

    add DTOs

---
+++ Contest_central.Application/DTOs/Logs/Validator/CreateLogValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contest_central.Application.DTOs.Logs.Validator
+{
+    internal class CreateLogValidator
+    {
+    }
+}
