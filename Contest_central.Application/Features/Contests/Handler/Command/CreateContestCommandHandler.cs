6dc84b91806a6ee16c0ac91fb9d492585da5b36a
abdulsemed <xbebe346@gmail.com>
2023-12-21 17:23:00 +0300

    add features and handler for contest entity

---
+++ Contest_central.Application/Features/Contests/Handler/Command/CreateContestCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using Contest_central.Application.DTOs.Contests.Validator;
+using Contest_central.Application.Features.Contests.Request.Command;
+using Contest_central.Application.Persistence.Contracts;
+using Contest_central.Application.Responses;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contest_central.Application.Features.Contests.Handler.Command
+{
+    public class CreateContestCommandHandler : IRequestHandler<CreateContestCommand, BaseResponseClass>
+    {
+        private readonly IContestRepository _contestrepository ;
+        public CreateContestCommandHandler(IContestRepository contestrepository)
+        {
+            this._contestrepository = contestrepository;
+        }
+        public async Task<BaseResponseClass> Handle(CreateContestCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateContestValidator(_contestrepository);
+            var validationResult = await validator.ValidateAsync(request.CreateContestDto, cancellationToken);
+            var response = new BaseResponseClass();
+            if (validationResult.IsValid)
+            {
+                response.Message = "Contest created succesfully";
+                response.Success = true;
+                response.Error = new List<string>();
+            }
+            else
+            {
+                response.Success= false;
+                response.Message = "Contest creation failed";
+                response.Error = validationResult.Errors.Select(x => x.ErrorMessage).ToList();
+            }
+
+            return response;
+        }
+    }
+}
---
+++ Contest_central.Application/Features/Contests/Handler/Query/GetContestRequestHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Contest_central.Application.DTOs.Contests;
+using Contest_central.Application.Features.Contests.Request.Query;
+using Contest_central.Application.Persistence.Contracts;
+using Contest_central.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contest_central.Application.Features.Contests.Handler.Query
+{
+    public class GetContestRequestHandler : IRequestHandler<GetContestRequest, ContestDto>
+    {
+        private readonly IContestRepository _contestRepository;
+        private readonly IMapper _mapper;
+
+        public GetContestRequestHandler(IContestRepository contestRepository, IMapper mapper)
+        {
+            this._contestRepository = contestRepository;
+            this._mapper = mapper;
+        }
+
+        public async Task<ContestDto> Handle(GetContestRequest request, CancellationToken cancellationToken)
+        {
+            var contest = await _contestRepository.GetById(request.ContestId);
+            return _mapper.Map<ContestDto>(contest);
+        }
+    }
+}
