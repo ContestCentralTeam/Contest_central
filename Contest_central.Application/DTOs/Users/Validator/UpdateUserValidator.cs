87cca28fe1ec2d64ede6e4f929edb6d385354de9
kibrom-hailu <kibrom.hailu@a2sv.org>
2023-12-21 17:49:47 +0300

    Finish the user validator

--- Contest_central.Application/DTOs/Users/Validator/UpdateUserValidator.cs
+++ Contest_central.Application/DTOs/Users/Validator/UpdateUserValidator.cs
@@ -4,6 +4,16 @@ namespace Contest_central.Application.DTOs.Users.Validator
 {
     public class UpdateUserValidator : AbstractValidator<CreateUserDto>
     {
+        private readonly IUserValidator _userValidator;
+        
+        public UpdateUserValidator(IUserValidator userValidator)
+        {
+            _userValidator = userValidator;
+            Include(new IUserValidator());
+            RuleFor(u => u.Id)
+            .NotEmpty().WithMessage("{PropertyName} must required");
+
+        }
 
     }
 }
